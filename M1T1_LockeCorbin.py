8bc2156d20400e0bdc1b3e2490ad916f50391686
lockec8556 <43530034+lockec8556@users.noreply.github.com>
2018-09-30 22:26:28 -0400

    First submission

---
+++ M1T1_LockeCorbin.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ('Hello world')
+Hello world
+>>> 
+ RESTART: C:/Users/Ageislion/Desktop/Corbin FTCC/CTI 110/P1T1_LockeCorbin.py 
+Hello World
+>>> 
